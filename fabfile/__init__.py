68e2c97a4881c59b1ce07b58bb49b47a5fa18191
mominosin <s_suzuking@icloud.com>
2015-10-09 19:16:00 +0900

    add comment

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -4,8 +4,10 @@ from fabric.decorators import task,runs_once
 
 import collections
 
+# pkginfo.pyの読み込み
 import pkginfo
 
+#サーバ2台
 env.hosts = ['test1','test2']
 env.user = 'ec2-user'
 env.key_filename = ['~/.ssh/test.pem']
@@ -17,14 +19,16 @@ def main():
     _get_host_pkg()
 
 def _get_host_pkg():
+    # env.host に実行ホストのホスト名が入る
     env.host_pkg_lists[env.host] = pkginfo.main()
 
 def _count_pkg():
     for host_pkgs in env.host_pkg_lists.itervalues():
         for pkg in host_pkgs:
            env.pkg_lists_sum[pkg] += 1
+
 @task
-@runs_once
+@runs_once #１回だけ実行
 def print_result():
   _count_pkg()
   server_count = len(env.hosts)
@@ -32,24 +36,3 @@ def print_result():
       print 'pkg_name_version: ' + k
       print '  pkgcount(installd/all_server): ' + str(v) + '/' + str(server_count) + '\n'
 
-
-
-#import sys
-#
-#import setting
-#
-#@task
-#def search():
-##    __import__('fabfile.' +env.pyfile)
-#    import ls
-#    print sys.modules.keys()
-#
-#    lists = getattr(__import__('fabfile.' + env.pyfile),env.method)()
-#    for f in lists:
-#        local('file %s' % f)
-#
-#@task
-#def lsearch(path):
-#    lists = local('ls -1 %s' % path,capture=True)
-#    for f in lists.split('\n'):
-#        local('file %s%s' % (path,f))
